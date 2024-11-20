362c0a0014363753e808bbdbf85104cb0ca4b7ac
Humayun1318 <humayunKabir6267@gmail.com>
2024-11-20 22:44:00 +0600

    student created feature done

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { StudentRoutes } from "./app/modules/student/student.route";
 const app: Application = express();
 // const port = 3000;
 
+//parser
 app.use(express.json());
-
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  let b;
-  const a = 6
-  res.send('Hello World!');
-});
+//application routes
+app.use("/api/v1/students", StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  res.send("Hello World!");
+};
+
+app.get("/", getAController);
 
 export default app;
---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: "male" | "female";
+  dateOfBirth: string;
+  email: string;
+  contact: string;
+  emergencyContactNo: string;
+  bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  avatar?: string;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: "active" | "blocked";
+};
